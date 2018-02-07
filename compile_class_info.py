a42320249680201004c4847c64ddcbf755a564df
Tiffany Wu <tiffwu88@gmail.com>
2018-02-07 11:08:24 -0500

    Update code, add everyone.csv, and *.json

--- compile_class_info.py
+++ compile_class_info.py
@@ -1,17 +1,76 @@
+# Import libraries
+import os
+import pandas as pd
+
+
 def main():
-    filenames = collect_csv_files()
-    student_data = cat_data()
-    write_data()
+    Filenames = collect_csv_files()
+    Everyone = cat_data(Filenames)
+    read_csv(Everyone)
+    write_data(Everyone)
+
 
 def collect_csv_files():
-    pass
+    import glob
+    # Collect all except mlp6
+    filenames = glob.glob('*.csv')
+    filenames.remove('mlp6.csv')
+    if 'everyone.csv' in filenames:     # This breaks code. What is another way to rewrite everyone.csv?
+        os.remove('everyone.csv')
+    else:
+        pass
+    return filenames
 
-def cat_data():
-    pass
 
-def write_():
+def cat_data(Filenames):
+    ind_data = []  #initialize for dataframe
+    for f in Filenames:  # More efficent way to do this?
+        df = pd.read_csv(f, delimiter=',', index_col=None, header=None)
+        ind_data.append(df)
+    everyone = pd.concat(ind_data, ignore_index=True)
+    everyone.columns = ['First', 'Last', 'NetID', 'GitHub', 'Teamname']
+    everyone = everyone.drop([2])
+    everyone.to_csv('everyone.csv', index=False)
+    return everyone
+
+
+def read_csv(Everyone):
+    check_no_spaces(Everyone)
+    check_camel_case(Everyone)
+
+
+def check_no_spaces(Everyone):
+    for index, row in Everyone.iterrows():
+        if ' ' in row[-1]:
+            print('Error: Space in Team name.')
+            # Refine to ignore first space, remove other spaces, and put in camelcase
+        else:
+            print(row[-1])
+            pass
+
+
+def check_camel_case(Everyone):
+    total_teams = Everyone.drop_duplicates(subset='Teamname')
+    camel_count = 0
+    for index, row in total_teams.iterrows():
+        if row[-1].istitle():  # No uppercase after lowercase
+            pass
+        else:
+            camel_count +=1
+    print('\n CamelCase {} out of {} teams.'.format(camel_count, len(total_teams.index)))
+    # Debug: Make sure teamname is consistent between team members
+
+
+def write_data(Everyone):
     # CSV or JSON
-    pass
+    for index, row in Everyone.iterrows():
+        name = row[-3]
+        row.to_json(name + '.json')
+    # Other possible method?
+    #name = Everyone['NetID'].to_string(index_names=False)
+    #print(name)
+
+
 
 if __name__ == "__main__":
     main()
